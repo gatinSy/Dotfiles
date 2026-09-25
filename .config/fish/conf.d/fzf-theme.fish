4b4644f54cb3d4d3d7faef8c9613275da6e7ae0e
Gatin <233465525+gatinSy@users.noreply.github.com>
2026-09-24 22:15:33 -0300

    .

--- .config/fish/conf.d/fzf-theme.fish
+++ .config/fish/conf.d/fzf-theme.fish
@@ -1,6 +1,6 @@
 set -Ux FZF_DEFAULT_OPTS "
-	--color=fg:#908caa,bg:-1,hl:#ea9a97
-	--color=fg+:#e0def4,bg+:#393552,hl+:#ea9a97
-	--color=border:#44415a,header:#3e8fb0,gutter:#232136
+	--color=fg:#908caa,bg:#191724,hl:#ebbcba
+	--color=fg+:#e0def4,bg+:#26233a,hl+:#ebbcba
+	--color=border:#403d52,header:#31748f,gutter:#191724
 	--color=spinner:#f6c177,info:#9ccfd8
 	--color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa"
